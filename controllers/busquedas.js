ac5618fc79c05df397b9aca28830a16fbb4b2fdb
Marcus Erick <marcushp1@hotmail.com>
2026-05-09 10:13:27 -0600

    feat: search collections

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -26,6 +26,44 @@ const getTodo = async(req,res = response) => {
   });
 }
 
+const getDocumentosColeccion = async(req,res = response) => {
+
+  const busqueda = req.params.busqueda;
+  const tabla = req.params.tabla;
+  const regex = new RegExp( busqueda, 'i' );
+
+  let data = [];
+
+  switch (tabla) {
+    case 'medicos':
+      data = await Medico.find({ nombre: regex })
+                    .populate('usuario', 'nombre img')
+                    .populate('hospital', 'nombre img')
+      break;
+    case 'hospitales':
+      data = await Hospital.find({ nombre: regex })
+                    .populate('usuario', 'nombre img')
+      break;
+    case 'usuarios':
+      data = await Usuario.find({ nombre: regex })
+                    .populate('usuario', 'nombre img')
+      break;
+  
+    default:
+      res.status(400).json({
+        ok: false,
+        msg: 'La tabla tiene que ser usuarios/medicos/hospitales'
+      })
+      break;
+  }
+
+  res.json({
+    ok: true,
+    resultados: data
+  });
+
+}
+
 module.exports = {
-  getTodo
+  getTodo, getDocumentosColeccion
 }
--- routes/busquedas.js
+++ routes/busquedas.js
@@ -3,14 +3,15 @@ const {Router} = require('express');
 const {check} = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos')
 
-// const { getUsuarios, crearUsuario, actualizarUsuario, borrarUsuario } = require('../controllers/usuarios')
-const { getTodo } = require('../controllers/busquedas')
+const { getTodo, getDocumentosColeccion } = require('../controllers/busquedas')
 
 const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
 router.get('/:busqueda', validarJWT ,getTodo);
+
+router.get('/coleccion/:tabla/:busqueda', validarJWT ,getDocumentosColeccion);
 
 
 
